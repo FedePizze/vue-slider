5a9171a8f5e2d09b27cb229f69b2867d37f07e2d
Federico Pizzeghello <pizzeghello.work@gmail.com>
2022-02-11 10:01:17 +0100

    aggiunta bonus 1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
 
 
+
 const app = new Vue({
     el: '#app',
     data: {
@@ -30,7 +31,7 @@ const app = new Vue({
                     name:'Paradise',
                     text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
                 }
-                ],
+            ],
 
         active: 0,
         autoScroll: null,
@@ -61,6 +62,10 @@ const app = new Vue({
 
         },
 
+        slide(indice) {
+            this.active = indice
+        },
+
         set() {
 
             this.autoScroll = setInterval(() => {
@@ -88,6 +93,11 @@ const app = new Vue({
                 }, 1000);
             }
 
+        },
+
+        show() {
+
+            show
         }
     },
 
@@ -95,81 +105,5 @@ const app = new Vue({
 
         this.set()
     },
-    
+
 });
-
-
-
-
-
-
-
-
-/*
-const itemsRef = document.getElementsByClassName('items')[0];
-const thumbsRef = document.getElementsByClassName('thumbs')[0];
-let item = '';
-let thumb = '';
-let active = 1;
-
-for (let i = 0; i < items.length; i++) {
-    item += `
-        <div class="item">
-            <img src="${items[i].url}" alt="">
-            <div class="text">
-                <h3>${items[i].name}</h3>
-                <p>${items[i].text}</p>
-            </div>
-        </div>`
-    thumb += `
-        <div class="thumb">
-            <img src="${items[i].url}" alt="">
-        </div>
-    `
-}
-
-itemsRef.innerHTML = item;
-document.getElementsByClassName('item')[active].classList.add('active');
-
-thumbsRef.innerHTML += thumb;
-document.getElementsByClassName('thumb')[active].classList.add('active');
-
-const prev = document.querySelector('.prev');
-prev.addEventListener('click', function() {
-    if(active == 0) {
-        active = items.length - 1; 
-
-        document.querySelector('.item.active').classList.remove('active');
-        document.getElementsByClassName('item')[active].classList.add('active');
-
-        document.querySelector('.thumb.active').classList.remove('active');
-        document.getElementsByClassName('thumb')[active].classList.add('active');
-    } else if(active < items.length) {
-        --active
-        document.querySelector('.item.active').classList.remove('active');
-        document.getElementsByClassName('item')[active].classList.add('active');
-
-        document.querySelector('.thumb.active').classList.remove('active');
-        document.getElementsByClassName('thumb')[active].classList.add('active');
-    } 
-});
-
-const next = document.querySelector('.next');
-next.addEventListener('click', function() {
-    if(active < items.length - 1) {
-        ++active
-        document.querySelector('.item.active').classList.remove('active');
-        document.getElementsByClassName('item')[active].classList.add('active');
-
-        document.querySelector('.thumb.active').classList.remove('active');
-        document.getElementsByClassName('thumb')[active].classList.add('active');
-    } else if(active == items.length - 1) { 
-        active = 0;
-        document.querySelector('.item.active').classList.remove('active');
-        document.getElementsByClassName('item')[active].classList.add('active');
-
-        document.querySelector('.thumb.active').classList.remove('active');
-        document.getElementsByClassName('thumb')[active].classList.add('active');
-    }
-});
-*/
